a684a978b513d20b59d5e070fd7d5a86094f8dab
brgndy <brgndy96@gmail.com>
2024-10-21 01:52:32 +0900

    fix: 빌드시 발생하는 에러 수정

--- maeil_mail/src/app/page.tsx
+++ maeil_mail/src/app/page.tsx
@@ -1,30 +1,5 @@
-'use client';
-
-import SecretModalTrigger from '@/_components/common/Modal/ModalContent/SecretModalTrigger';
-import SubscribeModal from '@/_components/Home/SubscribeModal/SubscribeModal';
-import HomeContent from '@/_components/Home/HomeContent';
-import useModal from '@/_hooks/useModal';
-import { myStyle } from '@/_styles/vars.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-export const client = new QueryClient({
-  defaultOptions: {
-    queries: {
-      throwOnError: true,
-    },
-  },
-});
+import HomePage from '@/_pages/HomePage';
 
 export default function Home() {
-  const { isModalOpen, handleModalOpen, handleModalClose } = useModal();
-
-  return (
-    <QueryClientProvider client={client}>
-      <div style={{ minWidth: '480px' }} className={myStyle}>
-        <HomeContent openModal={handleModalOpen} />
-        <SubscribeModal isOpen={isModalOpen} closeModal={handleModalClose} />
-        <SecretModalTrigger />
-      </div>
-    </QueryClientProvider>
-  );
+  return <HomePage />;
 }
