a7b94432ecdf267ada7f191c07f0dc803983046d
hanyoung <phanyoung@naver.com>
2024-10-28 21:20:02 +0900

    chore: sitemap 추가

---
+++ src/_constants/frontendBaseUrl.ts
@@ -0,0 +1,3 @@
+const FRONTEND_BASE_URL = "https://www.maeil-mail.kr";
+
+export default FRONTEND_BASE_URL;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { MSWComponent } from "@/_mocks/MSWComponent";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import Head from "next/head";
+import FRONTEND_BASE_URL from "@/_constants/frontendBaseUrl";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
   title: "매일메일 - 기술 면접 질문 구독 서비스",
   description: "기술 면접 질문을 매일매일 메일로 보내드릴게요!",
   openGraph: {
-    url: "https://maeil-mail.kr",
+    url: FRONTEND_BASE_URL,
     siteName: "maeil-mail",
     images: [
       {
