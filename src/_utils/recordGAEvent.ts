f470f3bea43036108836a261a35307b59cd7a905
hanyoung <phanyoung@naver.com>
2024-10-28 17:30:32 +0900

    chore: GA 이벤트 추적 debug mode 해제

--- src/_utils/recordGAEvent.ts
+++ src/_utils/recordGAEvent.ts
@@ -1,6 +1,6 @@
 const recordGAEvent = (action: string) => {
   if (window !== undefined && window.gtag) {
-    window.gtag("event", action, { debug_mode: true });
+    window.gtag("event", action);
   }
 };
 
