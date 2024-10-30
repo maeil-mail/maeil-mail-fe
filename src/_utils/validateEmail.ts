1398447d72064f1f5809a3767782ea3db65c7bd6
hanyoung <phanyoung@naver.com>
2024-10-30 20:44:32 +0900

    fix: 이메일 정규표현식을 더 엄격하게 설정

--- src/_utils/validateEmail.ts
+++ src/_utils/validateEmail.ts
@@ -1,8 +1,9 @@
 export const validateEmail = (email: string): boolean => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
   const koreanRegex = /[ㄱ-ㅎㅏ-ㅣ가-힣]/;
 
-  return email.trim() !== '' && emailRegex.test(email) && !koreanRegex.test(email);
+  return email.trim() !== "" && emailRegex.test(email) && !koreanRegex.test(email);
 };
 
 export default validateEmail;
