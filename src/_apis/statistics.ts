2c834bd2270e4d4c32dafc08103bc9950da6c454
brgndy <brgndy96@gmail.com>
2024-11-09 23:38:12 +0900

    refactor: 구독자수 캐시 no-store 설정

--- src/_apis/statistics.ts
+++ src/_apis/statistics.ts
@@ -1,10 +1,10 @@
-import BASE_URL from "./baseUrl";
+import BASE_URL from './baseUrl';
 
 export const getSubscriberCount = async () => {
-  const response = await fetch(`${BASE_URL}/statistics/subscribe`);
+  const response = await fetch(`${BASE_URL}/statistics/subscribe`, { cache: 'no-store' });
 
   if (!response.ok) {
-    throw new Error("getSubscriberCount api 에러가 발생했습니다.");
+    throw new Error('getSubscriberCount api 에러가 발생했습니다.');
   }
 
   const { cumulativeCount } = await response.json();
