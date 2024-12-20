f393d617bbc9366d28302af8f232889edb024069
hanyoung <phanyoung@naver.com>
2024-12-20 17:23:02 +0900

    refactor: useDeviceWidth 내 매직넘버를 변수로 선언

--- src/_hooks/useDeviceWidth.ts
+++ src/_hooks/useDeviceWidth.ts
@@ -1,12 +1,14 @@
 import debounce from '@/_utils/debounce';
 import { useEffect, useState } from 'react';
 
+const DEBOUNCE_WAIT = 300;
+
 const useDeviceWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
 
   const handleResize = debounce(() => {
     setWidth(window.innerWidth);
-  }, 300);
+  }, DEBOUNCE_WAIT);
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
