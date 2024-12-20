151fad7b3bb2066c3cdd494fd80f32925990830f
hanyoung <phanyoung@naver.com>
2024-12-20 17:20:41 +0900

    feat: Paginator 구현

---
+++ src/_components/common/Paginator/paginator.css.ts
@@ -0,0 +1,38 @@
+import { primary } from '@/_styles/vars.css';
+import { style } from '@vanilla-extract/css';
+
+export const paginatorContainer = style({
+  display: 'flex',
+  fontSize: '1.6rem',
+  width: 'fit-content',
+  alignItems: 'center',
+  margin: '0 auto',
+});
+
+export const doubleArrowLeft = style({
+  width: '2rem',
+  display: 'block',
+  marginRight: '1rem',
+});
+
+export const doubleArrowRight = style({
+  width: '2rem',
+  display: 'block',
+  marginLeft: '1rem',
+});
+
+export const pageNumber = style({
+  width: '3.3rem',
+  height: '3.3rem',
+  margin: '0 0.5rem',
+  textAlign: 'center',
+  lineHeight: '3.3rem',
+  borderRadius: 200,
+  color: '#6b7684',
+  selectors: {
+    '&[aria-current="true"]': {
+      color: 'white',
+      backgroundColor: primary,
+    },
+  },
+});
--- src/app/question/mine/[email]/page.tsx
+++ src/app/question/mine/[email]/page.tsx
@@ -3,24 +3,29 @@ import PageInnerLayout from '@/_components/common/PageInnerLayout/PageInnerLayou
 import Nav from '@/_components/common/Nav/Nav';
 import MyQuestionListHeader from '@/_components/MyQuestionList/Header';
 import MyQuestionList from '@/_components/MyQuestionList/MyQuestionList';
+import Paginator from '@/_components/common/Paginator';
 
 type MyQuestionListProps = {
   params: Promise<{ email: string }>;
+  searchParams: Promise<{ page: string }>;
 };
 
-export default async function MyQuestionListPage({ params }: MyQuestionListProps) {
+export default async function MyQuestionListPage({ params, searchParams }: MyQuestionListProps) {
   const { email: rawEmail } = await params;
+  const { page: rawPage } = await searchParams;
 
   const email = decodeURIComponent(rawEmail);
+  const page = Number(rawPage) || 1;
 
-  const questions = await getMyQuestions(email);
+  const { totalPage, data } = await getMyQuestions(email, page);
 
   return (
     <div>
       <Nav />
       <PageInnerLayout>
         <MyQuestionListHeader email={email} />
-        <MyQuestionList questions={questions} />
+        <MyQuestionList questions={data} />
+        <Paginator currentPage={page} lastPage={totalPage} />
       </PageInnerLayout>
     </div>
   );
