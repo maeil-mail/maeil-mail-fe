dc02feeb7711d2e58f8a0a7c53808af7a2bd1f38
hanyoung <phanyoung@naver.com>
2024-12-22 21:34:11 +0900

    fix: useDeviceWidth 서버 사이드에서는 window를 호출하지 않도록 수정

--- src/_hooks/useDeviceWidth.ts
+++ src/_hooks/useDeviceWidth.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 const DEBOUNCE_WAIT = 300;
 
 const useDeviceWidth = () => {
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState<number>();
 
   const handleResize = debounce(() => {
     setWidth(window.innerWidth);
@@ -12,6 +12,8 @@ const useDeviceWidth = () => {
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
+    setWidth(window.innerWidth);
+
     return () => {
       window.removeEventListener('resize', handleResize);
     };
