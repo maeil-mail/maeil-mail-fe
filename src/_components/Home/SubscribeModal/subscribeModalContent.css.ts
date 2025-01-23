b8fc670baa87999c120b652d7569e4bf3f55833b
hanyoung <phanyoung@naver.com>
2025-01-23 12:29:28 +0900

    chore: 불필요한 import 문 삭제

--- src/_components/Home/SubscribeModal/subscribeModalContent.css.ts
+++ src/_components/Home/SubscribeModal/subscribeModalContent.css.ts
@@ -10,7 +10,7 @@ export const container = style({
   flexDirection: 'column',
   gap: '3.5rem',
   '@media': {
-    'screen and (max-width: 800px)': {
+    'screen and (max-width: 820px)': {
       width: '100%',
     },
   },
