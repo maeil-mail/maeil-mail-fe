6e64f8f9dd9b1fcad980c1131322c3b94a00d5ef
hanyoung <phanyoung@naver.com>
2025-02-05 16:58:54 +0900

    chore: 환경 변수 관리 파일 생성

---
+++ src/_constants/publicEnv.ts
@@ -0,0 +1,20 @@
+export const GITHUB_OAUTH_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID as string;
+
+export const GITHUB_OAUTH_REDIRECT_URI = process.env
+  .NEXT_PUBLIC_GITHUB_OAUTH_REDIRECT_URI as string;
+
+export const MAEIL_WIKI_HOME_URL = process.env.NEXT_PUBLIC_MAEIL_WIKI_HOME_URL as string;
+
+export const MAEIL_WIKI_ERROR_PAGE_URL = process.env
+  .NEXT_PUBLIC_MAEIL_WIKI_ERROR_PAGE_URL as string;
+
+const envs = [
+  GITHUB_OAUTH_CLIENT_ID,
+  GITHUB_OAUTH_REDIRECT_URI,
+  MAEIL_WIKI_HOME_URL,
+  MAEIL_WIKI_ERROR_PAGE_URL,
+];
+
+if (envs.some((v) => !v)) {
+  throw new Error('일부 public 환경변수가 등록되지 않았습니다.');
+}
