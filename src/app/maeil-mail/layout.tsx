af11a1a5df34c48eee24dbe3a0ea965bf7d61f1c
hanyoung <phanyoung@naver.com>
2025-02-06 15:56:56 +0900

    feat: 매일위키 메타데이터 적용 및 파비콘 설정

---
+++ src/app/maeil-mail/layout.tsx
@@ -0,0 +1,31 @@
+import FRONTEND_BASE_URL from '@/_constants/frontendBaseUrl';
+import { Metadata } from 'next';
+import { PropsWithChildren } from 'react';
+
+export const metadata: Metadata = {
+  title: '매일메일 - 기술 면접 질문 구독 서비스',
+  description: '기술 면접 질문을 매일매일 메일로 보내드릴게요!',
+  openGraph: {
+    url: FRONTEND_BASE_URL,
+    siteName: 'maeil-mail',
+    images: [
+      {
+        url: 'https://dp71rnme1p14w.cloudfront.net/maeil-mail-ogImage.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'ko',
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+  verification: {
+    google: 'lUmlpDPafqt0it7egKAkKdPY_csrp9UO5iefrbN-4Rk',
+  },
+};
+
+export default function Layout({ children }: PropsWithChildren) {
+  return children;
+}
