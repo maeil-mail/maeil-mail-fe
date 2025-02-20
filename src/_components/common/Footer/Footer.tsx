e1e86f06766a774296bd73ec1df0f118f86e1bf9
hanyoung <phanyoung@naver.com>
2025-02-21 01:45:16 +0900

    design(Footer): 푸터 링크 호버 시 밑줄 표시되도록 스타일링

--- src/_components/common/Footer/Footer.tsx
+++ src/_components/common/Footer/Footer.tsx
@@ -1,10 +1,5 @@
-import {
-  GITHUB_ORGANIZATION_URL,
-  MAEIL_MAIL_GOOGLE_FORM_URL,
-  TEAM_EMAIL,
-  MAEIL_MAIL_INTRODUCTION_URL,
-} from '@/_constants/externalUrl';
-import { container, innerWrapper, side } from './footer.css';
+import { GITHUB_ORGANIZATION_URL, TEAM_EMAIL } from '@/_constants/externalUrl';
+import { container, externalLink, innerWrapper, side } from './footer.css';
 
 interface FooterProps {
   serviceName: string;
@@ -19,17 +14,24 @@ export default function Footer({ serviceName, introduceUrl, feedbackUrl }: Foote
         <section className={side}>
           <p>Copyright © 2025, {serviceName}. All rights reserved.</p>
           <div>
-            <a href={`mailto:${TEAM_EMAIL}`}>이메일 {TEAM_EMAIL}</a>
+            <a href={`mailto:${TEAM_EMAIL}`}>
+              이메일 <span className={externalLink}>{TEAM_EMAIL}</span>
+            </a>
           </div>
         </section>
         <section className={side}>
-          <a href={introduceUrl} target="_blank" rel="noopener noreferrer">
+          <a className={externalLink} href={introduceUrl} target="_blank" rel="noopener noreferrer">
             <span>팀 소개</span>
           </a>
-          <a href={feedbackUrl} target="_blank" rel="noopener noreferrer">
+          <a className={externalLink} href={feedbackUrl} target="_blank" rel="noopener noreferrer">
             <span>서비스 피드백하기</span>
           </a>
-          <a href={GITHUB_ORGANIZATION_URL} target="_blank" rel="noopener noreferrer">
+          <a
+            className={externalLink}
+            href={GITHUB_ORGANIZATION_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span>Github 스타는 서비스 운영에 큰 도움이 됩니다</span>
           </a>
         </section>
