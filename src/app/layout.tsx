8886b9682ad9ad37d4321594f3917da64f056fc0
hanyoung <phanyoung@naver.com>
2025-02-23 03:19:14 +0900

    fix: Head 대신 head 태그 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={pretendard.className}>
-      <Head>
+      <head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-      </Head>
+      </head>
       <body className={myStyle}>
         <div className={wrapper}>
           <Providers>
