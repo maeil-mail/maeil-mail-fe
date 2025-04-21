be6625a5f391bc0d17cd8272e10314df7f08ec13
sungun <gorjs3540@gmail.com>
2025-04-22 01:15:49 +0900

    feat: MailFrequency type 구현 및 적용

--- apps/maeilmail/src/components/setting/apis/getMailFrequency.ts
+++ apps/maeilmail/src/components/setting/apis/getMailFrequency.ts
@@ -1,8 +1,12 @@
 import mainClient from '@/common/apis/client/mainClient';
 import { pathGenerator } from '@/common/apis/constants/routes';
 
+interface MailFrequency {
+  frequency: 'daily' | 'weekly';
+}
+
 export const getMailFrequency = async (email: string) => {
-  const data = await mainClient.get(pathGenerator.myMailFrequency(email));
+  const data = await mainClient.get<MailFrequency>(pathGenerator.myMailFrequency(email));
 
   return data;
 };
