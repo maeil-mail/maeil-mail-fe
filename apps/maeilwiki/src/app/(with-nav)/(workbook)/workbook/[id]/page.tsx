11ec4cd6656e3a65b2cee806531275d52d7694d8
hanyoung <phanyoung@naver.com>
2025-05-05 22:38:42 +0900

    fix: 빌드 에러 해결

--- apps/maeilwiki/src/app/(with-nav)/(workbook)/workbook/[id]/page.tsx
+++ apps/maeilwiki/src/app/(with-nav)/(workbook)/workbook/[id]/page.tsx
@@ -6,29 +6,10 @@ import {
 } from '@/components/workbookDetail/workbookDetail.css';
 import { getWorkbookDetail } from '@/components/workbookDetail/apis/getWorkbookDetail';
 
-const MOCK = {
-  workbookTitle:
-    '자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구 자바스크립트 문제집 어쩌구',
-  difficultyLevel: 3,
-  category: 'backend',
-  workbookDetail:
-    '자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다. 자바스크립트 문제집입니다.',
-  owner: {
-    id: 2,
-    profileImage: 'string',
-    name: '박한영(Ryan)',
-    github: 'string',
-  },
-  createdAt: '2025-05-04',
-  timeLimit: 15,
-  questionCount: 3,
-  solvedCount: 345,
-} as const;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const id = (await params).id;
 
-export default async function Page({ params }: { params: { id: number } }) {
-  const id = params.id;
-
-  const workbook = await getWorkbookDetail(id);
+  const workbook = await getWorkbookDetail(Number(id));
 
   return (
     <div className={workbookDetailPageContainer}>
