c29fcb51936d3d0f87f1d26363c3209d77242d4d
hanyoung <phanyoung@naver.com>
2025-05-11 14:17:03 +0900

    refactor(maeilwiki): 불필요한 route 폴더 제거

--- apps/maeilwiki/src/app/(with-footer)/layout.tsx
+++ apps/maeilwiki/src/app/(with-footer)/layout.tsx
@@ -5,10 +5,12 @@ import {
 import { Footer } from '@maeil/ui';
 import * as React from 'react';
 import { withFooterContent } from '../layout.css';
+import MaeilWikiNav from '@/common/components/MaeilWikiNav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <MaeilWikiNav />
       <div className={withFooterContent}>{children}</div>
       <Footer
         serviceName="매일위키"
